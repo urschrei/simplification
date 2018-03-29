9a17afc83bb486db222c07f72dfaae123038a7e0
Stephan Hügel <urschrei@gmail.com>
2018-03-29 15:13:03 +0100

    Version bump

--- simplification/util.py
+++ simplification/util.py
@@ -37,7 +37,7 @@ from ctypes import Structure, POINTER, c_void_p, c_size_t, c_double, cast, cdll
 import numpy as np
 
 __author__ = u"Stephan Hügel"
-__version__ = "0.2.11"
+__version__ = "0.3.2"
 
 file_path = os.path.dirname(__file__)
 
