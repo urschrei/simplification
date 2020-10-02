15f9ab1353ef833c9128f45ced74e3bc7fd8113f
Stephan Hügel <shugel@tcd.ie>
2020-10-02 17:17:21 +0100

    Update release script with Windows 3.8 64-bit wheel
    [ci skip]

--- release.py
+++ release.py
@@ -27,6 +27,7 @@ releases = [
     {"tag": tag, "target": "x86_64-apple-darwin-cp37", "extension": "tar.gz"},
     {"tag": tag, "target": "x86_64-apple-darwin-cp38", "extension": "tar.gz"},
     {"tag": tag, "target": "x86_64-pc-windows-msvc-cp37", "extension": "zip"},
+    {"tag": tag, "target": "x86_64-pc-windows-msvc-cp38", "extension": "zip"},
     {"tag": tag, "target": "i686-pc-windows-msvc-cp37", "extension": "zip"},
     {"tag": tag, "target": "i686-pc-windows-msvc-cp27", "extension": "zip"},
     {"tag": tag, "target": "x86_64-unknown-linux-gnu", "extension": "tar.gz"},
