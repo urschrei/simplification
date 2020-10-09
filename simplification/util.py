4c6716674839fae0a52e2cf3de0de80d349cc942
Stephan Hügel <shugel@tcd.ie>
2020-10-09 12:13:27 +0100

    Use latest librdp

--- simplification/util.py
+++ simplification/util.py
@@ -37,7 +37,7 @@ from ctypes import Structure, POINTER, c_void_p, c_size_t, c_double, cast, cdll
 import numpy as np
 
 __author__ = u"Stephan Hügel"
-__version__ = "0.5.2"
+__version__ = "0.5.3"
 
 file_path = os.path.dirname(__file__)
 
