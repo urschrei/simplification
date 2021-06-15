37e874a7cb1247f85af2c903b76cb4a744a5e581
Stephan Hügel <shugel@tcd.ie>
2021-06-15 12:01:56 +0100

    Re-enable cutil tests

--- simplification/test/test_simplification.py
+++ simplification/test/test_simplification.py
@@ -4,12 +4,12 @@ import unittest
 import numpy as np
 import numpy
 from simplification.util import simplify_coords, simplify_coords_vw
-from simplification.util import simplify_coords as csimplify_coords
 from simplification.cutil import simplify_coords as csimplify_coords
-# from simplification.cutil import simplify_coords_idx as csimplify_coords_idx
-# from simplification.cutil import simplify_coords_vw as csimplify_coords_vw
-# from simplification.cutil import simplify_coords_vw_idx as csimplify_coords_vw_idx
-# from simplification.cutil import simplify_coords_vwp as csimplify_coords_vwp
+from simplification.cutil import simplify_coords as csimplify_coords
+from simplification.cutil import simplify_coords_idx as csimplify_coords_idx
+from simplification.cutil import simplify_coords_vw as csimplify_coords_vw
+from simplification.cutil import simplify_coords_vw_idx as csimplify_coords_vw_idx
+from simplification.cutil import simplify_coords_vwp as csimplify_coords_vwp
 
 
 class PolylineTests(unittest.TestCase):
@@ -33,11 +33,11 @@ class PolylineTests(unittest.TestCase):
 
         self.empty = []
 
-    # def testSimplify_rdp_numpy(self):
-    #     """ Test that numpy arrays can be consumed and returned """
-    #     npcoords = np.array(self.coords)
-    #     result = csimplify_coords(npcoords, 1.0)
-    #     self.assertEqual(type(result), numpy.ndarray)
+    def testSimplify_rdp_numpy(self):
+        """ Test that numpy arrays can be consumed and returned """
+        npcoords = np.array(self.coords)
+        result = csimplify_coords(npcoords, 1.0)
+        self.assertEqual(type(result), numpy.ndarray)
 
     def testSimplify_rdp(self):
         """ Test that a LineString can be simplified using RDP (Ctypes) """
@@ -60,43 +60,43 @@ class PolylineTests(unittest.TestCase):
         for _ in range(100):
             self.assertEqual(result, expected)
 
-    # def testCSimplify_rdp_idx(self):
-    #     """ Test that a LineString can be simplified using RDP (Cython) """
-    #     expected = self.result_rdp_idx
-    #     result = csimplify_coords_idx(self.coords, 1.0)
-    #     for _ in range(100):
-    #         self.assertEqual(result, expected)
+    def testCSimplify_rdp_idx(self):
+        """ Test that a LineString can be simplified using RDP (Cython) """
+        expected = self.result_rdp_idx
+        result = csimplify_coords_idx(self.coords, 1.0)
+        for _ in range(100):
+            self.assertEqual(result, expected)
 
-    # def testCSimplify_vw(self):
-    #     """ Test that a LineString can be simplified using VW (Cython) """
-    #     expected = self.resultvw
-    #     result = csimplify_coords_vw(self.coordsvw, 30.0)
-    #     for _ in range(100):
-    #         self.assertEqual(result, expected)
+    def testCSimplify_vw(self):
+        """ Test that a LineString can be simplified using VW (Cython) """
+        expected = self.resultvw
+        result = csimplify_coords_vw(self.coordsvw, 30.0)
+        for _ in range(100):
+            self.assertEqual(result, expected)
 
-    # def testCSimplify_vw_idx(self):
-    #     """ Test that a LineString can be simplified using VW (Cython) """
-    #     expected = self.resultvw_idx
-    #     result = csimplify_coords_vw_idx(self.coordsvw, 30.0)
-    #     for _ in range(100):
-    #         self.assertEqual(result, expected)
+    def testCSimplify_vw_idx(self):
+        """ Test that a LineString can be simplified using VW (Cython) """
+        expected = self.resultvw_idx
+        result = csimplify_coords_vw_idx(self.coordsvw, 30.0)
+        for _ in range(100):
+            self.assertEqual(result, expected)
 
-    # def testCSimplify_vw_preserve(self):
-    #     """ Test that a LineString can be simplified using topology-preserving VW (Cython) """
-    #     expected = self.resultvw
-    #     result = csimplify_coords_vwp(self.coordsvw, 30.0)
-    #     for _ in range(100):
-    #         self.assertEqual(result, expected)
+    def testCSimplify_vw_preserve(self):
+        """ Test that a LineString can be simplified using topology-preserving VW (Cython) """
+        expected = self.resultvw
+        result = csimplify_coords_vwp(self.coordsvw, 30.0)
+        for _ in range(100):
+            self.assertEqual(result, expected)
 
-    # def testCSingle_rdp(self):
-    #     """ Test that a one-element LineString is returned unaltered from RDP (Cython) """
-    #     result = csimplify_coords(self.single, 1.0)
-    #     self.assertEqual(result, self.single)
+    def testCSingle_rdp(self):
+        """ Test that a one-element LineString is returned unaltered from RDP (Cython) """
+        result = csimplify_coords(self.single, 1.0)
+        self.assertEqual(result, self.single)
 
-    # def testCSingle_vw(self):
-    #     """ Test that a one-element LineString is returned unaltered from VW (Cython) """
-    #     result = csimplify_coords_vw(self.single, 1.0)
-    #     self.assertEqual(result, self.single)
+    def testCSingle_vw(self):
+        """ Test that a one-element LineString is returned unaltered from VW (Cython) """
+        result = csimplify_coords_vw(self.single, 1.0)
+        self.assertEqual(result, self.single)
 
     def testCEmpty_rdp(self):
         """ Test that an empty LineString is returned unaltered from RDP (Cython) """
